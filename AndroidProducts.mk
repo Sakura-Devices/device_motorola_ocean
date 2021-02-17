0264d274239337789f0f2890c22b8cdeb6b0d778
Deivid21 <david.parra.ignacio@gmail.com>
2021-02-16 23:57:22 -0300

    ocean: AOSiP-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_ocean.mk
+    $(LOCAL_DIR)/aosip_ocean.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_ocean-eng \
-    lineage_ocean-user \
-    lineage_ocean-userdebug
+    aosip_ocean-eng \
+    aosip_ocean-user \
+    aosip_ocean-userdebug

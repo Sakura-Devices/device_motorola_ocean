47bcc11ebb573b11bf5c7938f255e4b21b123bf5
DeinMathelehrer <bruno@suedhoehe25.de>
2021-05-01 12:13:04 +0200

    ocean: cleanup config.mk cleaned some deperecated stuff from config.mk and corrected 1 syntax error

--- config.mk
+++ config.mk
@@ -1,9 +1,7 @@
 # Sakura stuff you know :D
 SAKURA_MAINTAINER := hisoka_simp
-# SAKURA_BUILD_TYPE := gapps
-# SAKURA_OPLAUNCHER is deprecated
 SAKURA_OFFICIAL := false
 
 # Other Rom specific stuff
 TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_USES_BLUR=true
+TARGET_USES_BLUR := true
